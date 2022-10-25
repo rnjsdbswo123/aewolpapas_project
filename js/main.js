1571a513358ca5c6e51ce1bc5e9e9a846e7a0473
rnjsdbswo123 <64054467+rnjsdbswo123@users.noreply.github.com>
2022-10-25 10:57:46 +0900

    메뉴리스트 구현 및 반응형 만들기

---
+++ js/main.js
@@ -0,0 +1,20 @@
+// 메뉴 리스트 구현
+var menu_li = ['메뉴 구현1', '메뉴 구현2', '메뉴 구현3', '메뉴 구현4']
+var shoping_ptn = ['구매하러 가기']
+console.log(menu_li[0])
+function print_Menu_li(){
+    var item_li_append =""
+    for (var i = 0; i < menu_li.length; i++) {
+        var find_li_item = document.getElementsByClassName('menu_1_ul')[0]
+        item_li_append += '<li><a class="menu_1_item" href="#">' + menu_li[i] + '</a></li>'
+        
+    }
+    find_li_item.innerHTML = item_li_append
+
+}
+// 메뉴 리스트 구현 끝
+
+
+// 함수 실행
+print_Menu_li()
+
