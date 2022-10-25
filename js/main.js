42a28a51a52ded77a9d4e771026a30c6592d88b4
rnjsdbswo123 <64054467+rnjsdbswo123@users.noreply.github.com>
2022-10-25 15:50:27 +0900

    pc,모바일 탑배너 완성

--- js/main.js
+++ js/main.js
@@ -4,17 +4,24 @@ var shoping_ptn = ['구매하러 가기']
 console.log(menu_li[0])
 function print_Menu_li(){
     var item_li_append =""
-    for (var i = 0; i < menu_li.length; i++) {
-        var find_li_item = document.getElementsByClassName('menu_1_ul')[0]
-        item_li_append += '<li><a class="menu_1_item" href="#">' + menu_li[i] + '</a></li>'
-        
+    for(var a = 0; a < 2; a++){
+        item_li_append=""
+        var find_li_item = document.getElementsByClassName('menu_1_ul')[a]
+        for (var i = 0; i < menu_li.length; i++) {
+                
+                item_li_append += '<li><a class="menu_1_item" href="#">' + menu_li[i] + '</a></li>'
+        }
+        find_li_item.innerHTML = item_li_append
     }
-    find_li_item.innerHTML = item_li_append
+}
 
+function mobileMenuOn(){
+    document.getElementById('mobile_menu').style.display="flex"
+}
+function mobileMenuOff(){
+    document.getElementById('mobile_menu').style.display="none"
 }
 // 메뉴 리스트 구현 끝
-
-
 // 함수 실행
 print_Menu_li()
 
